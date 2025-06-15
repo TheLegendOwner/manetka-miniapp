4feded9b87a1d3af9b00cbd486ad5103f446af78
fab3rok <fab3rok@gmail.com>
2025-06-15 04:17:48 +0300

    some fixes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['t.me', 'telegram.org', 'tonapi.io'], // уже были
+    domains: ['t.me', 'telegram.org', 'tonapi.io', 'cache.tonapi.io'], // уже были
   },
   async rewrites() {
     return [
